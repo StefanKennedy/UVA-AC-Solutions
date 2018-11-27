f9038ca21709eba48df89a6509be4a07f4ce8cfc
Stefan Kennedy <stefan.kennedy48@mail.dcu.ie>
2018-11-27 10:26:40 +0000

    Added solution to UVa 11172 Relational Operators

---
+++ 11172RelationalOperators/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    scanner.nextLine();
+    while (scanner.hasNext()) { // Try large input
+      int a = scanner.nextInt();
+      int b = scanner.nextInt();
+      if (a == b) {
+        System.out.println("=");
+      }
+      if (a > b) {
+        System.out.println(">");
+      }
+      if (a < b) {
+        System.out.println("<");
+      }
+    }
+  }
+
+}
